ca483939b808686b082c400e359aff76535a7084
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-10-24 10:46:22 +0200

    951 flip equivalent binary trees

--- src/problems/binary_tree_inorder_traversal.rs
+++ src/problems/binary_tree_inorder_traversal.rs
@@ -1,27 +1,9 @@
-use std::rc::Rc;
+use crate::problems::common_struct::TreeNode;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 #[allow(dead_code)]
 struct Solution;
-
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
-    }
-  }
-}
 
 impl Solution {
     #[allow(dead_code)]
@@ -39,6 +21,6 @@ impl Solution {
                 current = rc_node.borrow().right.clone();
             }
         }
-        return result
+        return result;
     }
 }
