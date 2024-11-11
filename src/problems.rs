052c19ebfa18b551a376998ea54b95de2ed87ee8
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-11-11 10:50:13 +0100

    2601 prime substraction operation

--- src/problems.rs
+++ src/problems.rs
@@ -82,6 +82,7 @@ mod number_complement;
 mod number_of_senior_citizens;
 mod path_max_probability;
 mod permutation_in_string;
+mod prime_subtraction_operation;
 mod range_sum_of_sorted_sub_arrays_sums;
 mod rangk_transform_of_array;
 mod regions_by_slashes;
