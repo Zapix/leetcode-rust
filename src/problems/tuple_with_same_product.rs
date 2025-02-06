c16a45b6b145039fbf689dab07046b42fce91132
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2025-02-06 10:58:14 +0100

    1726 tuple with same product

---
+++ src/problems/tuple_with_same_product.rs
@@ -0,0 +1,25 @@
+use std::collections::{HashMap, HashSet};
+#[allow(dead_code)]
+struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn tuple_same_product(nums: Vec<i32>) -> i32 {
+        let mut product_map = HashMap::new();
+        for i in 0..nums.len() {
+            for j in i+1..nums.len() {
+                let product = nums[i] * nums[j];
+                let count = product_map.entry(product).or_insert(0);
+                *count += 1;
+            }
+        }
+
+        product_map.iter().fold(0, |acc, (_, &val)| {
+            acc + if val > 1 {
+                val * (val - 1) * 4
+            } else {
+                0
+            }
+        })
+    }
+}
